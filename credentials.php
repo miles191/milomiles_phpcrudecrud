9d96a1c5c6dc0a49a609d2c27830a4e11ef70cf4
Milo Miles <miles191@umn.edu>
2024-10-28 05:26:58 +0000

    initial commit

---
+++ credentials.php
@@ -0,0 +1,7 @@
+<?php
+//Remeber to populate the credentials
+$servername = '';     //if apache on diff server from db or using Docker, set IP address
+$dbname = '';              //which db you're going to use
+$username = '';
+$password = '';
+?>
